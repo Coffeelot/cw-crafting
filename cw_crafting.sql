70e081cf45454031698e36f93a65f4137392143b
Coffeelot <slutastalkamig@gmail.com>
2025-01-18 19:47:38 +0100

    Better sql instructions

--- cw_crafting.sql
+++ cw_crafting.sql
@@ -1,1 +1,5 @@
+-- FOR QB/QBOX
 ALTER TABLE `players` ADD `crafting_blueprints` LONGTEXT NOT NULL DEFAULT '[]';
+
+-- FOR OX
+ALTER TABLE `characters` ADD `crafting_blueprints` LONGTEXT NOT NULL DEFAULT '[]';
