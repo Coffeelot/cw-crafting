06ac1e5cbaff61b6095a156b64cad263ff14a0ae
Coffeelot <slutastalkamig@gmail.com>
2025-01-25 17:54:53 +0100

    fix: correctly add items

--- server/server.lua
+++ server/server.lua
@@ -20,15 +20,15 @@ RegisterNetEvent('cw-crafting:server:craftItem', function(recipe, item, crafting
                 if useDebug then print('skipping removal of', material, 'as its marked as keep') end
             end
 
-            if item.toItems ~= nil then
-                for toMat, toAmount in pairs(item.toItems) do
-                    addItem(src, toMat, toAmount*craftingAmount, item.metadata)
-                end
-                success = true
-            else
-                print('^1Recipe is not created correctly: Missing toItems^0', recipe)
+            
+        end
+        if item.toItems ~= nil then
+            for toMat, toAmount in pairs(item.toItems) do
+                addItem(src, toMat, toAmount*craftingAmount, item.metadata)
             end
-
+            success = true
+        else
+            print('^1Recipe is not created correctly: Missing toItems^0', recipe)
         end
     else
         TriggerClientEvent('cw-crafting:client:notify', src, 'You are lacking some of the items to craft this', 'error') -- also possible exploit, if you wanna kick someone add it after this
